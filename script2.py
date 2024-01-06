9e7c2b6acc16b41a01123f69d784194f60e376ca
Gabriel Barbosa <99455542+xbenioff5680x@users.noreply.github.com>
2024-01-06 12:55:50 -0500

    Create script2.py
    final

---
+++ script2.py
@@ -0,0 +1,38 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+
+emails = fetch_20newsgroups(categories = ['comp.sys.ibm.pc.hardware', 'rec.sport.hockey'])
+print(emails.target_names)
+
+# emails are stored in emails.data
+print(emails.data[5])
+
+# labels are stored in emails.target
+print(emails.target[5])
+
+# labels themselves are #s but they correspond to label names found at emails.target_names
+print(emails.target_names[1])
+# target of email 5 is 1, which corresponds to rec.sport.hockey
+
+# split data into training and test sets
+train_emails = fetch_20newsgroups(categories = ['comp.sys.ibm.pc.hardware', 'rec.sport.hockey', 'rec.autos'], subset = 'train', shuffle = True, random_state = 108)
+test_emails = fetch_20newsgroups(categories = ['rec.sport.baseball', 'rec.sport.hockey', 'rec.autos'], subset = 'test', shuffle = True, random_state = 108)
+
+# Create a counter object to transform emails into lists of word counts
+counter = CountVectorizer()
+counter.fit(test_emails.data + train_emails.data)
+
+# Make a list of the counts of our words in the training set
+train_counts = counter.transform(train_emails.data)
+test_counts = counter.transform(test_emails.data)
+
+# Create a naive bayes classifier object to train and test on
+classifier = MultinomialNB()
+
+classifier.fit(train_counts, train_emails.target)
+
+# .score() returns the accuracy of the classifier on the test data measuring the percentage of classifications a classifier correctly makes
+print(classifier.score(test_counts, test_emails.target))
+# When we change the categories the classifer does a worse job classifying the categories
+
